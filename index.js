54e53da5e092525bfb49893e9a51f7e326c1e324
Eduardo Batista <dudubmatos@gmail.com>
2025-05-21 13:12:32 -0300

    Build

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT;
+
+const app = express();
+app.use(cors());
+
+app.get("/divisao", (req, res) => {
+  const { num1, num2 } = req.query;
+
+  if (parseFloat(num2) === 0) {
+    return res.status(400).json({ result: "Divisão por zero" });
+  }
+
+  const result = parseFloat(num1) / parseFloat(num2);
+  res.json({ result });
+});
+
+app.listen(port, () => {
+  console.log(`Serviço de Soma rodando na porta ${port}`);
+});
